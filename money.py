8b9d0a949cf23c912df0d24b818524a99a23606a
isgodlight <115672247+isgodlight@users.noreply.github.com>
2022-10-12 19:29:52 -0400

    money
    roy g biv

---
+++ money.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 17:38:32 2022
+
+@author: scott baskonbitz
+"""
+
+import math
+import sys
+import math
+
+m = 13
+o = 2
+n = 14
+e = 5
+y = 3
+
+red=1
+orange=3
+yellow=2
+blue=4
+indigo= 5
+green = 6
+violet = 7
+infared = 8
+xray = 9
+
+
+
+
+p1 = red,yellow
+p2 = orange
+p3 = yellow
+p4 = blue
+p5= indigo
+
+print(m,o,n,e,y,p1,p2,p3,p4,p5)
+print(p1,p2,p3,p4,p5)
+print((red,orange),yellow, orange,blue,indigo)
+
+print((red,xray),indigo,xray)
+print(infared,indigo,red)
+print(green,(red,orange),(orange,red),xray,blue)
+print((red,yellow),orange,(red,infared),red,(red,orange),(red,xray))
+
+
