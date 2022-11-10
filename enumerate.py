094ca68be1ccdd98e852e59942d3d1776d9c110a
isgodlight <115672247+isgodlight@users.noreply.github.com>
2022-11-09 21:46:25 -0500

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  9 18:32:03 2022
+
+@author: Owner
+"""
+
+
+
+s1 = ['eat','sleep','repeat','only']
+s2 = 'yeild'
+ert = enumerate(s1)
+yut = enumerate(s2)
+
+print("return type:", type(ert))
+print(list(enumerate(s1)))
+print(list(enumerate(s2,2)))
+
+you = s1[0]
+
+print(ert)
